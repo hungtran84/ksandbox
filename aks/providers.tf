05b2d7a817f79e04d5434f28fa183eee0fc585ed
Hung Tran <hungtran177@gmail.com>
2023-04-02 15:06:25 +0000

    Correct format

--- aks/providers.tf
+++ aks/providers.tf
@@ -6,7 +6,7 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.40, < 4.0"
     }
-    random = {
+    random  = {
       source  = "hashicorp/random"
       version = "3.3.2"
     }
